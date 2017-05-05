bdb3689df99b7cb3a9fdc0a60dd3aaeb72f3e4c3
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2017-05-05 16:18:09 -0300

    Add spy test on problemView to assert parameters are passed by router (showView)
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -8,4 +8,10 @@ it('shows the landing page view when there is no hash', function() {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
 });
+
+it('passes the hash view parameters to the view function', function() {
+    spyOn(learnjs,'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+});
 });
